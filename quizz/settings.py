a8a84ebf05275638bb48cc22a32d053f31c16aee
Giovannisb <giovannisb59@gmail.com>
2021-08-10 22:11:10 -0300

    no message

--- quizz/settings.py
+++ quizz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hm6j)jc$5)2kmcycdh*v!9a-icsh#t&v%%1bsn7l55-+f43m&z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
